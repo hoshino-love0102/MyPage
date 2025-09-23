889951a338480f513457d8898c4666727a9c6bca
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-09-23 23:34:48 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/spacekeep/SpaceKeepApplication.java
@@ -0,0 +1,13 @@
+package com.spacekeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceKeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceKeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spacekeep/SpaceKeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacekeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceKeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
