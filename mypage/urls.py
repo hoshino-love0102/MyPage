238809b2d21b9b02497f3456ffd505dbe064c968
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-09-24 14:02:40 +0900

    feat(models): accounts app연결 & User 모델 추가

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.post_list, name='post_list'),
     path('about/', views.about, name='about'),
+    path("accounts/", include("accounts.urls")),
 ]
