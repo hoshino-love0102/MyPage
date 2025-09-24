d7b2ed35fb118428a3309c3ff0042e1558c75584
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-09-24 14:11:46 +0900

    feat(auth): 회원가입/로그인 View 구현 및 URL 라우팅 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.utils.timezone import now
+from datetime import timedelta
 
-# Create your views here.
+from .serializers import SignupSerializer, LoginSerializer
+
+class SignupView(generics.CreateAPIView):
+    serializer_class = SignupSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+        access = str(refresh.access_token)
+
+        response = Response({
+            "message": "회원가입 성공",
+            "access": access,
+        }, status=status.HTTP_201_CREATED)
+
+        response.set_cookie(
+            key="refresh_token",
+            value=str(refresh),
+            httponly=True,
+            secure=False,
+            samesite="Lax",
+            expires=now() + timedelta(days=7)
+        )
+
+        return response
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+        refresh = RefreshToken.for_user(user)
+        access = str(refresh.access_token)
+
+        response = Response({
+            "message": "로그인 성공",
+            "access": access,
+        }, status=status.HTTP_200_OK)
+
+        response.set_cookie(
+            key="refresh_token",
+            value=str(refresh),
+            httponly=True,
+            secure=False,
+            samesite="Lax",
+            expires=now() + timedelta(days=7)
+        )
+
+        return response
