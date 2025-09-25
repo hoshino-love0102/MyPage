c627015ccc6e00a4a3f3df1ece8dd96b0b8e3538
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-09-25 19:16:06 +0900

    chore: add CORS middleware and credentials support

--- mypage/settings.py
+++ mypage/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "accounts",
     'blog',
+    "corsheaders",
 ]
 
 REST_FRAMEWORK = {
@@ -72,6 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'mypage.urls'
@@ -149,3 +151,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # 로컬 React
+]
+
+CORS_ALLOW_CREDENTIALS = True
